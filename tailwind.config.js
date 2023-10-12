1cc7fc245fd3a242696f95b01764a40faacb564a
magyarmatyas <magyarmatyi0305@gmail.com>
2023-10-12 22:53:24 +0200

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html, js}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    fontFamily: {
+      body: ["Mohave", "sans-serif"],
+    },
+  },
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [],
+};
