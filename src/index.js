ccb556d11528ac9cfc475dc3c71fbe967f95c7e8
magyarmatyas <magyarmatyi0305@gmail.com>
2023-10-14 13:57:40 +0200

    updating contact

--- src/index.js
+++ src/index.js
@@ -80,3 +80,10 @@ links.forEach((link) => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+function onlyNumberKey(evt) {
+  // Only ASCII character in that range allowed
+  let ASCIICode = evt.which ? evt.which : evt.keyCode;
+  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) return false;
+  return true;
+}
